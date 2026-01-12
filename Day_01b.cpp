38dced0b1b75dc057603d533ada0b48e91aef545
Mor Almog <gm.bebel12@gmail.com>
2026-01-12 21:16:34 +0200

    Rename Day_01b files

---
+++ Day_01b.cpp
@@ -0,0 +1,45 @@
+#include "Day_01b.h"
+#include "utility.h"
+#include "io.h"
+
+#include <iostream>
+
+int day_01b() {
+	std::string rotations = util::file_string("Day_01_input.txt");
+	std::vector<int> rotation_vec;
+
+	util::parse_rotation_string(rotations, rotation_vec);
+	for (int i = 0; i < rotation_vec.size(); i++) {
+		std::cout << rotation_vec[i] << std::endl;
+	}
+	return password_calc(rotation_vec);
+}
+
+// for all n,m in N exist r<m,q in N so that n = qm+r
+static int password_calc(const std::vector<int>& vec) {
+	int password = 0;
+	int pos = START_POS;
+	//io::FileOutput day02("day_02.txt");	// test helper
+	for (int i = 0; i < vec.size(); i++) {
+		
+		// extract abs(remainder)
+		int r = vec[i] % 100;
+		
+		// pos > 0 if pos + r < 0 => according to intermediate value theorem pos == 0 at some point during the rotation
+		// pos > 0 if pos + r > 100 => by same theorem pos == 100 at some point in the rotation
+		if (pos && ((pos + r) < 0 || (pos + r) > 100))password++;
+		
+		// abs(remainder)
+		r = (r < 0 ? r + 100 : r);
+		
+		// extract abs(quotient)
+		int q = vec[i] / 100;
+		password += util::abs(q);
+		
+		// calc abs(pos) under Z_100
+		pos = (pos + r) % 100;
+		if (!pos) password++;
+	}
+
+	return password;
+}
---
+++ Day_01b.h
@@ -0,0 +1,7 @@
+#pragma once
+//#include <vector>
+
+constexpr int START_POS = 50;
+
+//static int password_calc(const std::vector<int>& vec);
+int day_01b();

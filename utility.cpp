71b0a2509b20e86d5c1aada3e2b55ee0f46ee118
Mor Almog <gm.bebel12@gmail.com>
2026-01-22 19:32:08 +0200

    Add new utility functions and rename existing helper

--- utility.cpp
+++ utility.cpp
@@ -9,8 +9,12 @@
 
 namespace util {
 
-	int abs(int num) {
-		return (num < 0 ? -num : num);
+	bool is_odd(int num) { return num % 2; }
+	bool is_even(int num) { return (num + 1) % 2; }
+	int abs(int num) { return (num < 0 ? -num : num); }
+
+	bool belongs_in_range(int num, int min_range, int max_range) {
+		return (num >= min_range && num <= max_range);
 	}
 
 	template <typename T>
@@ -42,13 +46,28 @@ namespace util {
 
 	// takes a string and converts to positive integer (for now correctness of string will be assumed)
 	// TODO - error proof + except throw
-	std::size_t convert_string_to_numbers(std::string_view s_num) {
+	std::size_t str_to_num(std::string_view s_num) {
 		std::size_t num = 0;
-		for (int i = 0; i < s_num.size(); i++) {
+		for (std::size_t i = 0; i < s_num.size(); i++) {
 			num *= 10;
 			num += s_num[i] - 48;
 		}
 		return num;
+	}
+
+	std::string num_to_str(std::size_t num) {
+		std::size_t digit;
+		std::string num_s = "";
+		char digit_c;
+		while (num) {
+			// exstract last digit
+			digit = num % 10;
+			num /= 10;
+			// convert to ASCII
+			digit_c = digit + 48;
+			num_s = digit_c + num_s;
+		}
+		return num_s;
 	}
 
 };
--- utility.h
+++ utility.h
@@ -6,10 +6,14 @@
 
 namespace util {
 
+	bool is_even(int num);
+	bool is_odd(int num);
 	int abs(int num);
+	bool belongs_in_range(int num, int min_range, int max_range);
 	void print_vec(std::vector<int>& vec);
 	std::string file_string(std::string file_name);
-	std::size_t convert_string_to_numbers(std::string_view s_num);
+	std::size_t str_to_num(std::string_view s_num);
+	std::string num_to_str(std::size_t num);
 	//void parse_rotation_string(std::string& rotations, std::vector<int>& vec);
 
 };

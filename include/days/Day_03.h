45e8a023f7c780e0413e1449bf6e121621e56722
Mor Almog <gm.bebel12@gmail.com>
2026-02-06 00:45:29 +0200

    Implement Day 03 solution

--- include/days/Day_03.h
+++ include/days/Day_03.h
@@ -1,4 +1,9 @@
 #pragma once
 #include <cstddef>
+#include <vector>
+#include <string>
 
 std::size_t day_03();
+static std::vector<std::string> parse_string_to_vec(const std::string& str);
+static std::size_t find_max_battery(const std::string& bank);
+static std::size_t find_max_index(const std::string& str, std::size_t i = 0);
--- src/days/Day_03.cpp
+++ src/days/Day_03.cpp
@@ -1,8 +1,64 @@
 #include "days/Day_03.h"
 #include "core/utility.h"
 #include "core/io.h"
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <stdexcept>
 
-std::size_t day_03() {
-    // TODO: read input/Day_03_input.txt and solve
-    return 0;
+using std::string;
+using std::size_t;
+
+static io::FileOutput file("day_03.txt");
+
+size_t day_03() {
+    string banks_str = util::file_string("input/Day_03_input.txt");
+    std::vector<string> banks = parse_string_to_vec(banks_str);
+    size_t sum = 0;
+    for (string bank : banks) {
+        file.out() << "current bank: " << bank << '\n';
+        sum += find_max_battery(bank);
+    }
+    return sum;
+}
+
+static size_t find_max_battery(const string& bank) {
+    size_t idx1 = find_max_index(bank);
+    size_t idx2;
+    if (idx1 + 1 == bank.size()) {
+        idx2 = idx1;
+        idx1 = find_max_index(bank.substr(0, bank.size() - 1));
+    }
+    else idx2 = find_max_index(bank, idx1 + 1);
+
+    string battery = { bank[idx1], bank[idx2] };
+    file.out() << "current battery: " << battery << '\n';
+    return util::str_to_num(battery);
+}
+
+static size_t find_max_index(const string& str, size_t i) {
+    char max = '1';
+    size_t idx = i;
+    for (; i < str.size(); i++) {
+        if (max < str[i]) {
+            max = str[i];
+            idx = i;
+        }
+    }
+    return idx;
+}
+
+static std::vector<string> parse_string_to_vec(const string& str) {
+    string curr = "";
+    std::vector<string> vec;
+    for (char c : str) {
+        if (c == '\n') {
+            vec.push_back(curr);
+            curr.clear();
+        }
+        else
+            curr += c;
+    }
+    if (!curr.empty()) vec.push_back(curr);
+    return vec;
 }
